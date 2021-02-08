43e4799237b7cb11c3f5c08170b512160be0b04e
mumu111111 <2646709035@qq.com>
2021-02-08 22:43:26 +0800

    layout组件

--- src/App.tsx
+++ src/App.tsx
@@ -6,82 +6,46 @@ import {
   // Link,
   Redirect
 } from 'react-router-dom';
-import styled from 'styled-components';
-import Nav from './components/Nav';
 
+import Layout from './components/Layout';
 
-const Wrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-`
-const Main = styled.div`
-  flex-grow: 1;
-  overflow: auto;  
-`
-
-// const Nav = styled.nav`
-//   border: 1px solid #000;
-//   > ul {
-//     display: flex;
-//     > li {
-//       width: 33.3333%;
-//       text-align:center;
-//       padding: 16px;
-//     }
-//   }  
-// `
 
 function App() {
   return (
     <Router>
-      <Wrapper>
-        <Main>
-          <Switch>
-            <Route path="/tags">
-              <Tags />
-            </Route>
-            <Route path="/money">
-              <Money />
-            </Route>
-            <Route path="/statistics">
-              <Statistics />
-            </Route>
-            <Redirect exact from="/" to="/money" />
-            <Route path="*">
-              <NoMatch />
-            </Route>
-          </Switch>
-        </Main>
-        {/* <Nav>
-          <ul>
-            <li>
-              <Link to="/tags">标签页</Link>
-            </li>
-            <li>
-              <Link to="/money">记账页</Link>
-            </li>
-            <li>
-              <Link to="/statistics">统计页</Link>
-            </li>
-          </ul>
-        </Nav> */}
-        <Nav />
 
+      <Switch>
+        <Route path="/tags">
+          <Tags />
+        </Route>
+        <Route path="/money">
+          <Money />
+        </Route>
+        <Route path="/statistics">
+          <Statistics />
+        </Route>
+        <Redirect exact from="/" to="/money" />
+        <Route path="*">
+          <NoMatch />
+        </Route>
+      </Switch>
 
-      </Wrapper>
     </Router>
   );
 }
 
 function Tags() {
   return (
-    <div>tags</div>
+    <Layout>
+      <h2>标签</h2>
+    </Layout>
   )
 }
 function Money() {
   return (
-    <div>Money</div>
+    <Layout>
+      <h2>记账</h2>
+    </Layout>
   )
 }
 
@@ -89,7 +53,9 @@ function Money() {
 
 function Statistics() {
   return (
-    <div>Statistics</div>
+    <Layout>
+      <h2>统计</h2>
+    </Layout>
   )
 }
 
