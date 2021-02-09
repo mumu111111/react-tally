02111dda45ab457b574e3dd60bf4fea26f0d2227
mumu111111 <2646709035@qq.com>
2021-02-09 20:58:00 +0800

    完成NotedSection UI

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -19,6 +19,26 @@ const TagsSection = styled.section`
 
 `
 const NotesSection = styled.section`
+    background: #f5f5f5;
+    padding: 0 16px;
+    font-size: 14px;
+    > label {
+        display: flex;
+        align-items: center;
+        > span {
+            margin-right: 16px;
+            white-space: nowrap; //不换行
+        }
+        > input {
+            display: block;
+            width: 1000%;
+            height: 72px;
+            background: none;
+            border: none;
+        } 
+    }
+
+
 
 `
 const CategorySection = styled.section`
@@ -45,7 +65,7 @@ function Money() {
             <NotesSection>
                 <label>
                     <span>备注</span>
-                    <input type="text"></input>
+                    <input type="text" placeholder="添加备注" />
                 </label>
             </NotesSection>
             {/* 切换项 */}
