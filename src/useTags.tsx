cc2fc83fcc1b434556496bcf6eff869d08477e90
mumu111111 <2646709035@qq.com>
2021-03-01 17:23:40 +0800

    tags数据持久化

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,15 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { createId } from 'lib/createdId';
-import Tags from 'views/Tags';
-const defaultTags = [
-    { id: createId(), name: '衣' },
-    { id: createId(), name: '食' },
-    { id: createId(), name: '住' },
-    { id: createId(), name: '行' },
-]
-const useTags = () => {
-    const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
+import { useUpdate } from './hooks/useUpdate'
 
+const useTags = () => {
+    const [tags, setTags] = useState<{ id: number; name: string }[]>([]);
+    useEffect(() => {
+        let localTags = JSON.parse(window.localStorage.getItem('tags') || '[]')
+        if (localTags.length === 0) {
+            localTags = [
+                { id: createId(), name: '衣' },
+                { id: createId(), name: '食' },
+                { id: createId(), name: '住' },
+                { id: createId(), name: '行' },
+            ]
+        }
+        setTags(localTags)
+    }, [])
+
+    useUpdate(() => {
+        window.localStorage.setItem('tags', JSON.stringify(tags))
+    }, [tags])
     const findTag = (id: number) => tags.filter(tag => tag.id === id)[0];
 
     //  获取下标
